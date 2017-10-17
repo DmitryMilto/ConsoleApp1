36c6de2b32fdfb6fdb306004eb678603681dcef4
andel.ada.satana <andel.ada.satana@gmail.com>
2017-10-17 17:49:39 +0300

    Добавьте файлы проекта.

---
+++ UnitTestWeb/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestWeb
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static IWebDriver driver;
+        [AssemblyInitialize]
+        public static void SetUpWeb(TestContext context)
+        {
+            driver = new ChromeDriver(@"C:\Users\Admin\Documents\Visual Studio 2017\Projects\UnitTestWeb\UnitTestWeb");
+            driver.Manage().Window.Maximize();
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver.Navigate().GoToUrl("http://github.com");
+            driver.FindElement(By.LinkText("Sign in")).Click();
+            driver.FindElement(By.Id("login_field")).SendKeys("testautomationuser");
+            driver.FindElement(By.Id("password")).SendKeys("testautomationuser");
+            driver.FindElement(By.Name("commit")).Click();
+        }
+    }
+}
