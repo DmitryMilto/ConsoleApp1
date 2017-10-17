bc0e685b4e9c228026f6cef0a99e6f3e5d6c829f
andel.ada.satana <andel.ada.satana@gmail.com>
2017-10-17 17:59:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver;
+            driver = new ChromeDriver(@"C:\Users\Admin\Documents\Visual Studio 2017\Projects\UnitTestWeb\UnitTestWeb");
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://github.com");
+            driver.FindElement(By.LinkText("Sign in")).Click();
+            driver.FindElement(By.Id("login_field")).SendKeys("testautomationuser");
+            driver.FindElement(By.Id("password")).SendKeys("testautomationuser");
+            driver.FindElement(By.Name("commit")).Click();
+
+        }
+    }
+}
